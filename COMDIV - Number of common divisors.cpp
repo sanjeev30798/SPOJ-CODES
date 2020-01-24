0ec67afb3d692b08681ec5108206de17c93bd621
Sanjeev Kumar <49332829+sanjeev30798@users.noreply.github.com>
2020-01-24 23:38:56 +0530

    Update COMDIV - Number of common divisors.cpp

--- COMDIV - Number of common divisors.cpp
+++ COMDIV - Number of common divisors.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);//fast input and output
     cin.tie(NULL);
     cout.tie(NULL);
 	int n;
