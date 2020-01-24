becf8e7e45e6534ee2268b7cee5ae415688cb0af
Sanjeev Kumar <49332829+sanjeev30798@users.noreply.github.com>
2020-01-24 22:37:42 +0530

    AC

---
+++ COMDIV - Number of common divisors.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+	int n;
+	cin>>n;
+	while(n--)
+	{
+	    long int a,b,count=0;
+	    int i,t1;
+	    cin>>a>>b;
+	    t1=__gcd(a,b);
+	    for(i=1;i<=sqrt(t1);i++)
+	    {
+	        if(t1%i==0)
+	        {
+	            if(t1/i==i)
+	            count++;
+	            else
+	            count+=2;
+	        }
+	    }
+	    printf("%lu\n",count);
+	}
+	return 0;
+}
