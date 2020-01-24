39c7119d9e3ba0367eab49db8b1e6e6e3c92d033
Sanjeev Kumar <49332829+sanjeev30798@users.noreply.github.com>
2020-01-24 22:41:34 +0530

    AC

---
+++ SHAKTI - SHAKTIMAN AND KILWISH.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    long int k;
+	    cin>>k;
+	    if(k%2==1)
+	    {
+	        cout<<"Sorry Shaktiman"<<endl;
+	    }
+	    else
+	    cout<<"Thankyou Shaktiman"<<endl;
+	}
+	return 0;
+}
